0f0ae69709b987b5c573e6dd177299ee02401526
Jeanne <77942476+jeanne8879@users.noreply.github.com>
2021-09-19 19:13:06 -1000

    E26 WOD Complete

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+function sumFor (lst) {
+  let sum = 0
+  for (let i = 0; i < lst.length; i++) {
+    sum += lst[i]
+  }
+  return sum
+}
+
+function sumWhile (lst) {
+  let i = 0
+  let sum = 0
+  while (i < lst.length) {
+    sum += lst[i]
+    i++
+  }
+  return sum
+}
+
+function sumRecursion (lst) {
+  if (lst.length === 0) {
+    return 0
+  }
+  else {
+    let first = lst.shift()
+    return first + sumRecursion(lst)
+  }
+}
+
+function sumTheSimpleWay (lst) {
+  return _.reduce(lst, function(memo, num){ return memo + num; }, 0);
+}
+let testlist = [1, 2, 3, 4]
+console.log(sumFor(testlist))
+console.log(sumWhile(testlist))
+console.log(sumRecursion(testlist))
+console.log(sumTheSimpleWay(testlist))
